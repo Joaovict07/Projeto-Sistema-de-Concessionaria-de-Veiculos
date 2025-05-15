03fb16e0dda0f1d19a104d8a1325b366cf0a70cd
Beatriz Sanches <bia.olive1234@gmail.com>
2025-05-15 20:49:11 -0300

    update

--- src/PF.java
+++ src/PF.java
@@ -1,2 +1,12 @@
-public class PF {
+public class PF extends Cliente {
+
+    protected String cpf;
+
+    public PF(String nome, String contato, String tipo, String cpf) {
+        super(nome, contato, tipo);
+        this.cpf = cpf;
+    }
+    public String getCpf() { return cpf; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
 }
