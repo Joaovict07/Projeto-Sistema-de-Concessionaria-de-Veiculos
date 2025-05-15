0dd175608dc8e2ae5f2cfe16e05cdf127c89137a
Joaovict07 <joaovictr204@gmail.com>
2025-05-15 20:36:00 -0300

    Classe Cliente, e PJ adicionada

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,36 @@
 public class Cliente {
+
+    protected String nome;
+    protected String contato;
+    protected String tipo;
+
+    public Cliente(String nome, String contato, String tipo) {
+        this.nome = nome;
+        this.contato = contato;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
--- src/PJ.java
+++ src/PJ.java
@@ -1,2 +1,17 @@
-public class PJ {
+public class PJ extends Cliente {
+
+    protected String cnpj;
+
+    public PJ(String nome, String contato, String tipo, String cnpj) {
+        super(nome, contato, tipo);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
