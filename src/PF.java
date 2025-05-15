1f840c77114afe8b0ea522276f08ff55f3c40a41
Beatriz Sanches <bia.olive1234@gmail.com>
2025-05-15 20:55:23 -0300

    update2

--- src/PF.java
+++ src/PF.java
@@ -1,12 +1,35 @@
 public class PF extends Cliente {
-
     protected String cpf;
+    protected int idade;
+    protected String endereco;
 
-    public PF(String nome, String contato, String tipo, String cpf) {
+    public PF(String nome, String contato, String tipo, String cpf, int idade, String endereco) {
         super(nome, contato, tipo);
         this.cpf = cpf;
     }
-    public String getCpf() { return cpf; }
 
-    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
 }
