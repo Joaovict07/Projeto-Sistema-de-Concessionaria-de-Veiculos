26056fe6c35b26fd173214096c9a3fe3e5854a91
Christian Ribeiro Gama Franco <chrisribgama3@gmail.com>
2025-05-15 21:18:44 -0300

    Adicionando a classe Veículo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Veiculo carro1 = new Carro();
+        carro1.setModelo("Civic");
+        carro1.setFabricante("Toyota");
+        carro1.setAno_fabricacao(2026);
+
+        carro1.exibirInformacoes();
+
     }
 }
